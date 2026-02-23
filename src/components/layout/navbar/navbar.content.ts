26b77de3a51b3cf98579ab3db34798aef9f1d399
Lautaa Blanco <lautaronicoblanco@hotmail.com>
2026-02-23 19:37:50 -0300

    fix: renaming folders with incorrect casing

---
+++ src/components/layout/navbar/navbar.content.ts
@@ -0,0 +1,9 @@
+import type { NavLink } from './navbar.types'
+
+export const navbarLinks: NavLink[] = [
+    { label: "Inicio", href: "#hero" },
+    { label: "Servicios", href: "#services" },
+    { label: "Nosotros", href: "#whyus" },
+    { label: "Proceso", href: "#process" },
+    { label: "Contacto", href: "#contact" },
+]
